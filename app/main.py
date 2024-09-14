05d136588b57468287596e0a5a10812f08e199ca
Ekram-20 <ekramferasj@gmail.com>
2024-09-14 08:55:14 +0300

    test: add nav

--- app/main.py
+++ app/main.py
@@ -1,21 +1,38 @@
 import streamlit as st
 
 
-home = st.Page(
+# pg = st.navigation([st.Page(''), st.Page("page2.py")])
+# pg.run()
 
-)
+pages = {
+    # "Your account": [
+    #     st.Page("create_account.py", title="Create your account"),
+    #     st.Page("manage_account.py", title="Manage your account"),
+    # ],
+    "مراجع": [
+        st.Page("learn.py", title=" تعلم "),
+        st.Page("trial.py", title="Try it out"),
+    ],
+}
+
+pg = st.navigation(pages, position='sidebar')
+pg.run()
+
 
 st.markdown("""
 <style>
 body, html {
     direction: RTL;
-    unicode-bidi: bidi-override;
     text-align: right;
 }
 p, div, input, label, h1, h2, h3, h4, h5, h6 {
     direction: RTL;
-    unicode-bidi: bidi-override;
     text-align: right;
 }
+
+.st-emotion-cache-1tokvoz eczjsme3 {
+    display: none;
+}
+
 </style>
 """, unsafe_allow_html=True)
